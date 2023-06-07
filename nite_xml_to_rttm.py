d7b3ad8a1577f9db2b3ffd38dfcb1ebf38d99418
mananjeet <85890048+mananjeet@users.noreply.github.com>
2023-06-08 01:56:00 +0530

    Update nite_xml_to_rttm.py

--- nite_xml_to_rttm.py
+++ nite_xml_to_rttm.py
@@ -1,45 +1,58 @@
 import math
-import sys
 import xml.etree.ElementTree as ET
+import os
 
+input_directory = 'XML'  # Specify the input directory here
+output_directory = 'rttm'  # Specify the output directory here
+os.makedirs(output_directory, exist_ok=True)
 xml_trees = []
 
-def print_rttm_line(id_value,start_time, end_time, speaker_id):
-    speaker_char = chr(ord('A') + speaker_id)  
-    print("SPEAKER {} 0 \t{:.2f} \t{:.2f}\t<NA>\t<NA>\t {}\t<NA>".format(id_value,start_time, end_time - start_time,
-                                                                            speaker_char))
+def print_rttm_line(output_file, id_value, start_time, end_time, speaker_id):
+    output_file.write("SPEAKER {} 0 \t{:.2f} \t{:.2f}\t<NA>\t<NA>\t {}\t<NA>\n".format(id_value, start_time,
+                                                                                      end_time - start_time,
+                                                                                      speaker_id))
 
 
-def convert_xml_to_rttm():
-    xml_file = xml_files[xml_file_index]
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-    xml_trees.append(root)
-    start_time = end_time = None
+def convert_xml_to_rttm(xml_files, output_file):
+    speaker_id_counter = 0
+    for xml_file in xml_files:
+        speaker_id = chr(ord('A') + speaker_id_counter)
+        speaker_id_counter += 1
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        xml_trees.append(root)
+        start_time = end_time = None
 
-    # Define the namespace
-    namespace = {"nite": "http://nite.sourceforge.net/"}
+        # Define the namespace
+        namespace = {"nite": "http://nite.sourceforge.net/"}
 
-    for element in root.iter("w"):
-        if 'starttime' in element.attrib and 'endtime' in element.attrib:
-            if start_time is None:
-                start_time = float(element.attrib['starttime'])
-            if end_time is None:
-                end_time = float(element.attrib['starttime'])  # yes, 'starttime'
-            if math.isclose(end_time, float(element.attrib['starttime']), abs_tol=0.01):
-                # collapse the two
-                end_time = float(element.attrib['endtime'])
-            else:
-                nite_id = element.attrib.get('{{{}}}id'.format(namespace["nite"]), '')
-                id_value = nite_id[:7]  # Extract first 4 letters
-                print_rttm_line(id_value, start_time, end_time, speaker_id=xml_file_index)
-                start_time = float(element.attrib['starttime'])
-                end_time = float(element.attrib['endtime'])
-    if not ((start_time is None) or (end_time is None)):
-        nite_id = root.attrib.get('{{{}}}id'.format(namespace["nite"]), '')
-        id_value = nite_id[:7]  # Extract first 4 letters
-        print_rttm_line(id_value, start_time, end_time, speaker_id=xml_file_index)
+        for element in root.iter("w"):
+            if 'starttime' in element.attrib and 'endtime' in element.attrib:
+                if start_time is None:
+                    start_time = float(element.attrib['starttime'])
+                if end_time is None:
+                    end_time = float(element.attrib['starttime'])  # yes, 'starttime'
+                if math.isclose(end_time, float(element.attrib['starttime']), abs_tol=0.01):
+                    # collapse the two
+                    end_time = float(element.attrib['endtime'])
+                else:
+                    nite_id = element.attrib.get('{{{}}}id'.format(namespace["nite"]), '')
+                    id_value = nite_id[:7].replace('.', '')  # Extract first 4 letters and remove '.'
+                    print_rttm_line(output_file, id_value, start_time, end_time, speaker_id)
+                    start_time = float(element.attrib['starttime'])
+                    end_time = float(element.attrib['endtime'])
+        if not ((start_time is None) or (end_time is None)):
+            nite_id = root.attrib.get('{{{}}}id'.format(namespace["nite"]), '')
+            id_value = nite_id[:7].replace('.', '')  # Extract first 4 letters and remove '.'
+            print_rttm_line(output_file, id_value, start_time, end_time, speaker_id)
 
-xml_files = sys.argv[1:]
-for xml_file_index in range(len(xml_files)):
-    convert_xml_to_rttm()
+
+for root, dirs, files in os.walk(input_directory):
+    for dir in dirs:
+        folder_path = os.path.join(root, dir)
+        xml_files = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if file.endswith('.xml')]
+        if not xml_files:
+            continue
+        output_file_path = os.path.join(output_directory, dir + '.rttm')
+        with open(output_file_path, 'w') as output_file:
+            convert_xml_to_rttm(xml_files, output_file)
